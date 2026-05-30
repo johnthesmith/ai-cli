e5c5fd542243b387d9059a717dccc14be2a4026b
Still Swamp <you@example.com>
2026-05-31 02:02:23 +0300

    Update ./github-model-list.sh

--- github-model-list.sh
+++ github-model-list.sh
@@ -2,5 +2,5 @@
 
 curl -L \
     -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer `cat ~/.config/ai/default/token.txt`" \
+    -H "Authorization: Bearer `cat ~/.config/ai/default/tokens/github.txt`" \
     https://models.github.ai/catalog/models | jq '.[] | .id'
